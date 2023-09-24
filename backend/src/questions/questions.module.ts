c83317762d973cf220a6e84db18f37c81de4838b
Flavio Bertolini <info@noobill.ch>
2023-09-24 23:18:34 +0200

    controlling if a question is from a quizz you created

--- backend/src/questions/questions.module.ts
+++ backend/src/questions/questions.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Question} from "questions/entities/question.entity";
+import {Quizz} from "quizzs/entities/quizz.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Question, Quizz])],
   controllers: [QuestionsController],
   providers: [QuestionsService],
   exports: [QuestionsService],
--- backend/src/questions/questions.service.ts
+++ backend/src/questions/questions.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
+import {Question} from "questions/entities/question.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Quizz} from "quizzs/entities/quizz.entity";
 
 @Injectable()
 export class QuestionsService {
+  constructor(
+      @InjectRepository(Question)
+      private readonly questionRepository: Repository<Question>,
+      @InjectRepository(Quizz)
+      private readonly quizzRepository: Repository<Quizz>,
+  ) {}
+
   create(createQuestionDto: CreateQuestionDto) {
     return 'This action adds a new question';
   }
 
-  findAll() {
-    return `This action returns all questions`;
+  async findAll(quizzId, user): Promise<Question[]> {
+    const quizz = await this.quizzRepository.findOne({ where: { id: quizzId, user: user } });
+    if (!quizz) {
+      throw new Error('Quizz not found or you do not have permission');
+    }
+
+    const question = await this.questionRepository.find({ where: { quizz: quizz } });
+    if (!question) {
+      throw new Error('Question not found');
+    }
+
+    return question;
   }
 
   findOne(id: number) {
