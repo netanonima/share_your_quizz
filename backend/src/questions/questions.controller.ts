039798ced6045af15b22179d975e6356d3291b59
Flavio Bertolini <info@noobill.ch>
2023-09-24 23:18:34 +0200

    controlling if a question is from a quizz you created

--- backend/src/questions/questions.controller.ts
+++ backend/src/questions/questions.controller.ts
@@ -12,6 +12,8 @@ import { QuestionsService } from './questions.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 import { JwtAuthGuard } from './../auth/guards/jwt-auth.guard';
+import {GetUser} from "decorators/user.decorator";
+import {User} from "users/entities/user.entity";
 
 @Controller('questions')
 export class QuestionsController {
@@ -25,8 +27,8 @@ export class QuestionsController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.questionsService.findAll();
+  findAll(@Param('quizzId') quizzId: number, @GetUser() user: User) {
+    return this.questionsService.findAll(quizzId, user);
   }
 
   @UseGuards(JwtAuthGuard)
