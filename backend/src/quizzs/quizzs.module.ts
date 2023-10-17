e84cd77f4c4a1dfb1d39257e5c8844f1e91dc75a
netanonima <altordj@gmail.com>
2023-10-17 11:22:09 +0200

    fixed a module relation issue

--- backend/src/quizzs/quizzs.module.ts
+++ backend/src/quizzs/quizzs.module.ts
@@ -3,9 +3,19 @@ import { QuizzsService } from './quizzs.service';
 import { QuizzsController } from './quizzs.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Quizz} from "quizzs/entities/quizz.entity";
+import {Question} from "questions/entities/question.entity";
+import {Choice} from "choices/entities/choice.entity";
+import {Media} from "medias/entities/media.entity";
+import {Image} from "images/entities/image.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quizz])],
+  imports: [TypeOrmModule.forFeature([
+    Quizz,
+      Question,
+      Choice,
+      Media,
+      Image
+  ])],
   controllers: [QuizzsController],
   providers: [QuizzsService],
 })
