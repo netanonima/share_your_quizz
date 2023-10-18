a22ed4e355795e90c568771e7bdebd59d46de160
netanonima <altordj@gmail.com>
2023-10-18 14:27:54 +0200

    consolidation pending

--- backend/src/questions/dto/update-question.dto.ts
+++ backend/src/questions/dto/update-question.dto.ts
@@ -1,6 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateQuestionDto } from './create-question.dto';
+import {IsNotEmpty, IsOptional, IsString} from "class-validator";
 
 export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
+    id?: number;
     deleted?: boolean;
+
+    @IsOptional()
+    @IsString()
+    readonly question: string | null;
 }
