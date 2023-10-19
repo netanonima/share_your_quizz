38b57485bf1b7217bd135ca1e6d4e96de6e910e0
netanonima <altordj@gmail.com>
2023-10-19 09:40:03 +0200

    test with angular

---
+++ frontend_angular/src/app/page-content/page-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-content',
+  templateUrl: './page-content.component.html',
+  styleUrls: ['./page-content.component.scss']
+})
+export class PageContentComponent {
+
+}
