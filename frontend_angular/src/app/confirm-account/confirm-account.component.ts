ea8f5e3690ca38bf5734c8dc69932c33c5fef390
Flavio Bertolini <info@noobill.ch>
2023-10-19 23:59:03 +0200

    confirm-account and reset-password added

---
+++ frontend_angular/src/app/confirm-account/confirm-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-account',
+  templateUrl: './confirm-account.component.html',
+  styleUrls: ['./confirm-account.component.scss']
+})
+export class ConfirmAccountComponent {
+
+}
