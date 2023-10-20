23532c1c5d3ec9a3cf6e93e60609229644fab201
Flavio Bertolini <info@noobill.ch>
2023-10-20 12:04:24 +0200

    token duration set

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -25,7 +25,7 @@ export class AuthService {
   async login(user: User) {
     const payload = { username: user.username, userId: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, { expiresIn: '4h'}),
     };
   }
 }
