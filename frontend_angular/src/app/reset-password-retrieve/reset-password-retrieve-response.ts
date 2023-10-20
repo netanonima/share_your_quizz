4735c67a8402f0f0f99e92fd157598c9ead50e81
Flavio Bertolini <info@noobill.ch>
2023-10-20 16:51:21 +0200

    reset password retrieve form implemented

---
+++ frontend_angular/src/app/reset-password-retrieve/reset-password-retrieve-response.ts
@@ -0,0 +1,6 @@
+export interface RegistrationResponse {
+  id: number;
+  username: string;
+  email: string;
+  created_on: Date;
+}
--- frontend_angular/src/app/reset-password-retrieve/reset-password-retrieve.component.ts
+++ frontend_angular/src/app/reset-password-retrieve/reset-password-retrieve.component.ts
@@ -1,10 +1,92 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ApiService} from "../api.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+import {RegistrationResponse} from "../register/registration-response";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-reset-password-retrieve',
   templateUrl: './reset-password-retrieve.component.html',
-  styleUrls: ['./reset-password-retrieve.component.scss']
+  styleUrls: ['./reset-password-retrieve.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({opacity: 1})),
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(600 )
+      ]),
+      transition(':leave',
+        animate(600, style({opacity: 0})))
+    ])
+  ]
 })
 export class ResetPasswordRetrieveComponent {
+  resetPasswordRetrieveForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(10), Validators.maxLength(70)]),
+  });
 
+  getErrorMessage(controlName: string): string {
+    const control = this.resetPasswordRetrieveForm.get(controlName);
+    if (control) {
+      if (control.hasError('required')) {
+        return 'You must enter a value';
+      }
+
+      if(control.hasError('minlength')) {
+        return 'Minimum length is ' + control.getError('minlength').requiredLength;
+      }
+
+      if(control.hasError('maxlength')) {
+        return 'Maximum length is ' + control.getError('maxlength').requiredLength;
+      }
+
+      return control.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    return '';
+  }
+
+  resetPasswordRetrieve() {
+    if (this.resetPasswordRetrieveForm.valid) {
+      console.log(this.resetPasswordRetrieveForm.value);
+      this.onSubmit();
+    }
+  }
+
+  cancel() {
+    // empty all fields
+    this.resetPasswordRetrieveForm.reset();
+  }
+
+  constructor(
+    private apiService: ApiService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
+
+  onSubmit() {
+    if (this.resetPasswordRetrieveForm.valid) {
+      const formData = this.resetPasswordRetrieveForm.value;
+      this.apiService.register(formData).subscribe(
+        (response: RegistrationResponse) => {
+          console.log('Reset password retrieve success', response);
+          this.snackBar.open('Reset password retrieve success, please check your e-mails', 'Close', {
+            panelClass: ['snackbar-success'],
+            duration: 6000
+          });
+          this.router.navigate(['/']);
+        },
+        (error: HttpErrorResponse) => {
+          console.error('Registration failed', error);
+          this.snackBar.open('An error occurred', 'Close', {
+            panelClass: ['snackbar-warning'],
+            duration: 3000
+          });
+        }
+      );
+    }
+  }
 }
