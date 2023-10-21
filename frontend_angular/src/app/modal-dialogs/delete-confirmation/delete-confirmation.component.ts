1daf6f3d58dba2992b1a4530318783d791e87dbc
Flavio Bertolini <info@noobill.ch>
2023-10-21 22:22:53 +0200

    quizzes component improvments

---
+++ frontend_angular/src/app/modal-dialogs/delete-confirmation/delete-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirmation',
+  templateUrl: './delete-confirmation.component.html',
+  styleUrls: ['./delete-confirmation.component.scss']
+})
+export class DeleteConfirmationComponent {
+
+}
