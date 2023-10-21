ddc7524b6566fe404baaa9354c4196a2f05828ce
Flavio Bertolini <info@noobill.ch>
2023-10-21 21:25:46 +0200

    quizzes implementation

--- backend/src/quizzs/quizzs.service.ts
+++ backend/src/quizzs/quizzs.service.ts
@@ -82,7 +82,7 @@ export class QuizzsService {
   async findAll(user: User): Promise<Quizz[]> {
     return this.quizzRepository.find({
       where: { user: { id: user.id }, deleted_on: null },
-      relations: ['questions', 'questions.choices', 'questions.media', 'questions.image']
+      select: ['id', 'quizz', 'created_on', 'modified_on']
     });
   }
 
--- frontend_angular/src/app/quizzes/quizzes.component.ts
+++ frontend_angular/src/app/quizzes/quizzes.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService} from "../api.service";
+import {Sort} from "@angular/material/sort";
+
+export interface Quizz {
+  id: number;
+  quizz: string;
+  created_on: Date;
+  modified_on: Date;
+}
 
 @Component({
   selector: 'app-quizzes',
   templateUrl: './quizzes.component.html',
-  styleUrls: ['./quizzes.component.scss']
+  styleUrls: ['./quizzes.component.scss'],
 })
-export class QuizzesComponent {
-  data: any;
+export class QuizzesComponent implements OnInit{
+  data: Quizz[] = [];
+  sortedData : Quizz[];
 
   constructor(private apiService: ApiService) {
+    this.sortedData = this.data.slice();
   }
 
   ngOnInit(): void {
@@ -22,5 +32,31 @@ export class QuizzesComponent {
         console.log(error);
       }
     )
+  }
+
+  sortData(sort: Sort) {
+    const data = this.data.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'quizz':
+          return compare(a.quizz, b.quizz, isAsc);
+        case 'created_on':
+          return compare(a.created_on, b.created_on, isAsc);
+        case 'modified_on':
+          return compare(a.modified_on, b.modified_on, isAsc);
+        default:
+          return 0;
+      }
+    });
+
+    function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
   }
 }
