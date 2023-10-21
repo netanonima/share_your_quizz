99e349a6c71cb6ccfbb95b710da2bb92a276ccff
Flavio Bertolini <info@noobill.ch>
2023-10-21 20:59:53 +0200

    quizzs backend fixes

--- backend/src/quizzs/dto/update-quizz.dto.ts
+++ backend/src/quizzs/dto/update-quizz.dto.ts
@@ -1,9 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateQuizzDto } from './create-quizz.dto';
-import {IsDateString, IsInt, IsNotEmpty, IsOptional} from "class-validator";
+import {IsDateString, IsInt, IsNotEmpty, IsOptional, IsString} from "class-validator";
 
 export class UpdateQuizzDto extends PartialType(CreateQuizzDto) {
     deleted?: boolean;
+
+    @IsOptional()
+    @IsString()
+    readonly quizz: string;
 
     @IsOptional()
     @IsDateString()
