db229e8c6cdd5b0757a4240c4dbdc3f22da5f11b
Flavio Bertolini <info@noobill.ch>
2023-10-22 02:43:16 +0200

    first socket files created

---
+++ backend/src/play-sockets/play-sockets.module.ts
@@ -0,0 +1,8 @@
+// sockets.module.ts
+import { Module } from '@nestjs/common';
+import { PlaySocketsGateway } from './play-sockets.gateway';
+
+@Module({
+    providers: [PlaySocketsGateway],
+})
+export class SocketsModule {}
