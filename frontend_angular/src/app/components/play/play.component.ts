5c86d97e2c8154bdfec587e432ea6bb6c7e25b99
Flavio Bertolini <info@noobill.ch>
2023-10-22 02:36:25 +0200

    mobile navbar bug fixes and play component prepare before socket tests

--- frontend_angular/src/app/components/play/play.component.ts
+++ frontend_angular/src/app/components/play/play.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ApiService} from "../../services/api/api.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-play',
@@ -12,27 +13,37 @@ export class PlayComponent implements OnInit, OnDestroy{
   constructor(
     private apiService: ApiService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    public authService: AuthService,
   ) {
   }
 
   ngOnInit(): void {
     const sessionId = this.route.snapshot.queryParamMap.get('id');
-    if(sessionId && sessionId!=null){
-      this.apiService.getSession(sessionId).subscribe(
-        (result) => {
-          console.log('Session retrieved');
-          console.log(result);
-          this.error = false;
-        },
-        (error) => {
-          console.log(error);
-          this.error = true;
-        }
-      );
+    if(this.authService.isAuthenticated()){
+      if(sessionId && sessionId!=null){
+        this.apiService.getSession(sessionId).subscribe(
+          (result) => {
+            console.log('Session retrieved');
+            console.log(result);
+            this.error = false;
+            // start session for host
+            console.log('Start session for host');
+          },
+          (error) => {
+            console.log(error);
+            this.error = true;
+          }
+        );
+      }else{
+        console.log('No session id provided');
+        this.error = true;
+      }
     }else{
-
+      // start session for players
+      console.log('Start session for players');
     }
+
   }
 
   ngOnDestroy() {
