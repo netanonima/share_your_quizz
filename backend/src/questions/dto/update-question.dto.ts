f6c4a1f979f60f80c5a5214b45118209d6c8f4d2
netanonima <altordj@gmail.com>
2023-10-23 16:25:26 +0200

    todo stuff when media is sent to question (update)

--- backend/src/questions/dto/update-question.dto.ts
+++ backend/src/questions/dto/update-question.dto.ts
@@ -1,5 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateQuestionDto } from './create-question.dto';
+import {IsOptional, IsString} from "class-validator";
 
 export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
+    @IsOptional()
+    @IsString()
+    readonly media: string;
 }
