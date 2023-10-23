394148ddac9531e743f31b19fa36e72c6b7b2e77
Flavio Bertolini <info@noobill.ch>
2023-10-23 21:45:43 +0200

    media upload and deleting on backend implemented

--- backend/src/medias/medias.module.ts
+++ backend/src/medias/medias.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MediasService } from './medias.service';
 import { MediasController } from './medias.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Media} from "medias/entities/media.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Media])],
   controllers: [MediasController],
   providers: [MediasService],
-  exports: [MediasService]
+  exports: [
+    MediasService,
+    TypeOrmModule
+  ]
 })
 export class MediasModule {}
