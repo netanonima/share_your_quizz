34bf252db4f44d85b2018ec2a6f534aac2ec03e8
netanonima <altordj@gmail.com>
2023-10-23 14:44:08 +0200

    component choices generated

---
+++ frontend_angular/src/app/components/choices/choices.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choices',
+  templateUrl: './choices.component.html',
+  styleUrls: ['./choices.component.scss']
+})
+export class ChoicesComponent {
+
+}
