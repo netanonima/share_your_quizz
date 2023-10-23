c4a115834de123588fc8fedbf4f0cd5ec107a433
netanonima <altordj@gmail.com>
2023-10-23 14:51:22 +0200

    backend: images deleted

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,14 +6,12 @@ import { User } from './users/entities/user.entity';
 import { Quizz } from './quizzs/entities/quizz.entity';
 import { Question } from './questions/entities/question.entity';
 import { Choice } from './choices/entities/choice.entity';
-import { Image } from './images/entities/image.entity';
 import { Media } from './medias/entities/media.entity';
 import { Session } from './sessions/entities/session.entity';
 import { UsersModule } from './users/users.module';
 import { QuizzsModule } from './quizzs/quizzs.module';
 import { QuestionsModule } from './questions/questions.module';
 import { ChoicesModule } from './choices/choices.module';
-import { ImagesModule } from './images/images.module';
 import { MediasModule } from './medias/medias.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { AuthModule } from './auth/auth.module';
@@ -28,7 +26,7 @@ import { PlaySocketsModule } from './play-sockets/play-sockets.module';
       username: 'phpmyadmin',
       password: 'phpmyadmin',
       database: 'share_your_quizz',
-      entities: [User, Quizz, Question, Choice, Image, Media, Session],
+      entities: [User, Quizz, Question, Choice, Media, Session],
       synchronize: true,
       autoLoadEntities: true,
       logging: true,
@@ -37,7 +35,6 @@ import { PlaySocketsModule } from './play-sockets/play-sockets.module';
     QuizzsModule,
     QuestionsModule,
     ChoicesModule,
-    ImagesModule,
     MediasModule,
     SessionsModule,
     AuthModule,
