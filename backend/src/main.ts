49bfb7ffe3b6e6ce8c261532d70507cc3bb86b0e
Flavio Bertolini <info@noobill.ch>
2023-10-29 05:52:48 +0100

    another deployment oriented changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 import {ConfigService} from "@nestjs/config";
+import * as fs from 'fs';
 
 async function bootstrap() {
+  /*
+  const httpsOptions = {
+    key: fs.readFileSync('/folder/your.key'),
+    cert: fs.readFileSync('/folder/your.crt'),
+  };
+  */
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe());
