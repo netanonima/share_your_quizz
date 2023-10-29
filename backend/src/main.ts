c8e68bb6e80486c446a46d9f014f299a2d8293b3
Flavio Bertolini <info@noobill.ch>
2023-10-29 04:37:20 +0100

    another deployment oriented changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe());
+  const configService = new ConfigService();
+
   app.enableCors({
     allowedHeaders: '*',
     origin: (origin, callback) => {
-      const configService = new ConfigService();
       const allowedOrigin = configService.get('FRONTEND_URL');
       if (!allowedOrigin || origin === allowedOrigin) {
         callback(null, true);
@@ -21,6 +22,6 @@ async function bootstrap() {
     },
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(configService.get('BACKEND_PORT'));
 }
 bootstrap();
