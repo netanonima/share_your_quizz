fe4afc4b9f1e3fb65d8996b282a8c5bb00358f17
Flavio Bertolini <info@noobill.ch>
2023-12-20 20:40:19 +0100

    error while receiving their own quizz only fixed

--- backend/src/quizzs/quizzs.service.ts
+++ backend/src/quizzs/quizzs.service.ts
@@ -44,8 +44,9 @@ export class QuizzsService {
   }
 
   async findAll(user: User): Promise<Quizz[]> {
+    console.log('current userid', user.id);
     return this.quizzRepository.find({
-      where: { user: user },
+      where: { user: { id: user.id } },
       select: ['id', 'quizz', 'created_on', 'modified_on', 'param_shuffle_questions', 'param_shuffle_choices'],
     });
   }
